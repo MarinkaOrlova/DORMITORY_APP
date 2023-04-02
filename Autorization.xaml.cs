0ef3f14a99e59c6a33a237d89d7fb812c935540d
omarina616@gmail.com <omarina616@gmail.com>
2023-04-02 14:02:01 +0300

    Добавьте файлы проекта.

---
+++ Autorization.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Automation;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using MessageBox = System.Windows.MessageBox;
+
+namespace SIMPLE_APP
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Autorization : Window
+    {
+        public Autorization()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Auto_Click(object sender, RoutedEventArgs e)
+        {
+            string login = TextBox_login.Text.Trim();
+            string pass = PasswordBox_password.Password.Trim();
+
+            if (login.Length < 5)
+            {
+                TextBox_login.ToolTip = "Ойойой, ошибочка вышла...";
+                TextBox_login.Background = Brushes.Linen;
+            }
+            else if (pass.Length < 5)
+            {
+                PasswordBox_password.ToolTip = "Ойойой, ошибочка вышла...";
+                PasswordBox_password.Background = Brushes.Teal;
+            }
+            else
+            {
+                /*TextBox_login.ToolTip = "";
+                TextBox_login.Background = Brushes.Transparent;
+                PasswordBox_password.ToolTip = "";
+                PasswordBox_password.Background = Brushes.Transparent;*/
+
+                User authuser = null;
+                using (AppContext db = new AppContext())
+                {
+                    authuser = db.Users.Where(b => b.Login == login && b.Pass == pass).FirstOrDefault();
+                }
+
+                if (authuser != null)
+                {
+                    MainWindow mainWindow= new MainWindow();
+                    mainWindow.Show();
+                    Hide();
+                }
+                else
+                    MessageBox.Show("Вы ввели что-то некорректно");
+            }
+        }
+
+        private void Button_R_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrationWindow registrationWindow = new RegistrationWindow();
+            registrationWindow.Show();
+            Hide();
+        }
+    }
+}
---
+++ RegistrationWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.Entity;
+
+namespace SIMPLE_APP
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class RegistrationWindow : Window
+    {
+        AppContext db;
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+
+            db = new AppContext();
+        }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = TextBox_login.Text.Trim();
+            string pass = PasswordBox_password.Password.Trim();
+            string pass_2 = PasswordBox_repeat_password.Password.Trim();
+            string email = TextBox_email.Text.Trim().ToLower();
+
+            if (login.Length < 5)
+            {
+                TextBox_login.ToolTip = "Ойойой, ошибочка вышла...";
+                TextBox_login.Background = Brushes.Linen;
+            }
+            else if (pass.Length < 5)
+            {
+                PasswordBox_password.ToolTip = "Ойойой, ошибочка вышла...";
+                PasswordBox_password.Background = Brushes.Teal;
+            }
+            else if (pass_2 != pass)
+            {
+                PasswordBox_repeat_password.ToolTip = "Ойойой, ошибочка вышла...";
+                PasswordBox_repeat_password.Background = Brushes.Teal;
+            }
+            else if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                TextBox_email.ToolTip = "Ойойой, ошибочка вышла...";
+                TextBox_email.Background = Brushes.Teal;
+            }
+            else
+            {
+                TextBox_login.ToolTip = "";
+                TextBox_login.Background = Brushes.Transparent;
+                PasswordBox_password.ToolTip = "";
+                PasswordBox_password.Background = Brushes.Transparent;
+                PasswordBox_repeat_password.ToolTip = "";
+                PasswordBox_repeat_password.Background = Brushes.Transparent;
+                TextBox_email.ToolTip = "";
+                TextBox_email.Background = Brushes.Transparent;
+
+                
+                User user = new User(login, email, pass);
+
+                db.Users.Add(user);
+                db.SaveChanges();
+
+                MessageBox.Show("SO GOOD!");
+
+                Autorization autorization = new Autorization();
+                autorization.Show();
+                Hide();
+            }
+        }
+
+        private void Button_wa_Click(object sender, RoutedEventArgs e)
+        {
+            Autorization autorization = new Autorization();
+            autorization.Show();
+            Hide();
+
+        }
+    }
+}
+       
